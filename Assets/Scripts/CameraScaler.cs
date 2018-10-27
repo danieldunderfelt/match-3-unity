e49a37dec36ea5b0ac357f9bc8ddb86830a3284f
Daniel Dunderfelt <daniel@developsuperpowers.com>
2018-10-27 20:16:32 +0300

    Make camera resize and center on the board.

---
+++ Assets/Scripts/CameraScaler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScaler : MonoBehaviour
+{
+  public float aspectRatio = 0.625f;
+  public float padding = 2;
+  public float zOffset;
+  private Board board;
+
+  void Start()
+  {
+    board = FindObjectOfType<Board>();
+
+    if (board != null) {
+      RepositionCamera(board.width - 1, board.height - 1);
+    }
+  }
+
+  void RepositionCamera(float x, float y)
+  {
+    Vector3 newPosition = new Vector3(x / 2, y / 2, zOffset);
+    transform.position = newPosition;
+
+    if (board.width >= board.height) {
+      Camera.main.orthographicSize = (board.width / 2 + padding) / aspectRatio;
+    } else {
+      Camera.main.orthographicSize = board.height / 2 + padding;
+    }
+  }
+
+  void Update()
+  {
+
+  }
+}
